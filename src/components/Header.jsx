c5c1b7786688a4e77936f93de5377132a35c1445
anas599 <anas599@yahoo.com>
2023-03-12 13:15:51 +0300

    Add header Component with icons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,35 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import SettingsIcon from '@mui/icons-material/Settings';
+import MicIcon from '@mui/icons-material/Mic';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import { cryptoInfoReset } from '../redux/crypto/cryptoSlice';
 
-const Header = () => (
-  <header>Header</header>
-);
+const Header = () => {
+  const dispatch = useDispatch();
+
+  const handleResetCryptoInfo = () => {
+    dispatch(cryptoInfoReset());
+  };
+
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <button type="button" onClick={handleResetCryptoInfo}>
+              -
+              <ChevronLeftIcon />
+            </button>
+
+          </li>
+          <li><p>Prices</p></li>
+          <li><MicIcon /></li>
+          <li><SettingsIcon /></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
